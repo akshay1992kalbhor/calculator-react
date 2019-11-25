a4ff4b3320a0078a2a6c4bba0bc83e41fe43684d
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2019-11-25 09:49:54 +0530

    add the line which imports the calculate func

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
 import './App.css';
 
 
@@ -12,7 +13,7 @@ export default class App extends React.Component {
   };
 
   handleClick = buttonName => {
-
+    this.setState(calculate(this.state, buttonName));
   };
 
   render() {
